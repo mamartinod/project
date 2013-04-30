0950c39c616beee5b74b8690512fbd645115a5ef
Ryan Varley <ryanjvarley@gmail.com>
2013-04-30 11:47:29 +0100

    restructured code and added a run file

--- fitsfile_extractor.py
+++ fitsfile_extractor.py
@@ -1,12 +1,19 @@
 import pyfits
 
-class FitsExtractor:
+
+class FitsFile:  # this will hold a fitsfile so its better to call it a fitsfile
     
     def __init__(self, fits_file):
         with pyfits.open(fits_file) as dataFile:
-            self.header = dataFile[0].header
-            self.data = dataFile[1].data
-            self.zero = dataFile[-5].data
+            self.loadfile(dataFile)
+
+    def loadfile(self, dataFile):
+        """ I created this method to split the code up a bit and allow me to create a unit test for loading without
+        having to make a mock fits file
+        """
+
+        self.header = dataFile[0].header
+        self.data = self.zeroReadSubstract(dataFile[1].data, dataFile[-5].data)
             
-    def zeroReadSubstract(self):
-        self.data = self.data - self.zero
+    def zeroReadSubstract(self, data, zeroRead):
+        return data - self._zero
---
+++ run.py
@@ -0,0 +1,8 @@
+""" module to run the code in the class
+"""
+
+from fitsfile_extractor import FitsFile
+
+fits = FitsFile("ibxy01aoq_raw.fits")
+
+
