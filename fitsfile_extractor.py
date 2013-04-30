7e56efa2bba83cc3df66074b3edf508de162ee8a
marc-antoine <ma.martinod@free.fr>
2013-04-30 10:59:12 +0100

    FitsExtractor. Added zeroRead method.

--- fitsfile_extractor.py
+++ fitsfile_extractor.py
@@ -6,3 +6,7 @@ class FitsExtractor:
         with pyfits.open(fits_file) as dataFile:
             self.header = dataFile[0].header
             self.data = dataFile[1].data
+            self.zero = dataFile[-5].data
+            
+    def zeroReadSubstract(self):
+        self.data = self.data - self.zero
