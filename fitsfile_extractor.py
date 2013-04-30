0c28dd642e7e1c166c2b86d4581f4ef5476c81f9
Ryan Varley <ryanjvarley@gmail.com>
2013-04-30 11:56:35 +0100

    added skeleton test code

--- fitsfile_extractor.py
+++ fitsfile_extractor.py
@@ -1,7 +1,7 @@
 import pyfits
 
 
-class FitsFile:  # this will hold a fitsfile so its better to call it a fitsfile
+class FitsFile(object):  # this will hold a fitsfile so its better to call it a fitsfile
     
     def __init__(self, fits_file):
         with pyfits.open(fits_file) as dataFile:
@@ -9,11 +9,11 @@ class FitsFile:  # this will hold a fitsfile so its better to call it a fitsfile
 
     def loadfile(self, dataFile):
         """ I created this method to split the code up a bit and allow me to create a unit test for loading without
-        having to make a mock fits file
+        having to make a actual fits file
         """
 
         self.header = dataFile[0].header
         self.data = self.zeroReadSubstract(dataFile[1].data, dataFile[-5].data)
             
     def zeroReadSubstract(self, data, zeroRead):
-        return data - self._zero
+        return data - zeroRead
---
+++ tests/test_fitsfile_extractor.py
@@ -0,0 +1,21 @@
+import unittest
+
+
+class Test_FitsFile(unittest.TestCase):
+
+    def zeroReadSubstract(self):
+
+        # TODO create two arrays, open a FitsFile object, and pass them through it
+
+        # Hint: read up on unittests and let the FitsFile class work if no fits file is given to it at first
+
+        answer = None
+        expected = False
+
+        self.assertTrue(answer == expected)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
