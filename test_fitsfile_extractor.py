5c963f3c335cbfae36449331167d29f2ac92eceb
marc-antoine <ma.martinod@free.fr>
2013-04-30 17:15:19 +0100

    change directory of test_fitsfile

--- test_fitsfile_extractor.py
+++ test_fitsfile_extractor.py
@@ -6,8 +6,9 @@ class Test_FitsFile(unittest.TestCase):
     def test_zeroReadSubstract(self):
 
         # TODO create two arrays, open a FitsFile object, and pass them through it
-
         # Hint: read up on unittests and let the FitsFile class work if no fits file is given to it at first
+        
+        
 
         answer = None
         expected = False
