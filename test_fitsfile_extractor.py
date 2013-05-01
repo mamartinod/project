08f22ea9051beca770a68a868bd14bd19a58a4e3
marc-antoine <ma.martinod@free.fr>
2013-05-01 11:29:23 +0100

    I have to commit it before merging.

--- test_fitsfile_extractor.py
+++ test_fitsfile_extractor.py
@@ -1,17 +1,17 @@
 import unittest
-
+import fitsfile_extractor
 
 class Test_FitsFile(unittest.TestCase):
 
     def test_zeroReadSubstract(self):
-
+        fits.header
         # TODO create two arrays, open a FitsFile object, and pass them through it
         # Hint: read up on unittests and let the FitsFile class work if no fits file is given to it at first
         
         
 
         answer = None
-        expected = False
+        expected = None
 
         self.assertTrue(answer == expected)
 
