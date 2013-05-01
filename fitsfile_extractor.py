b93df3b5efb3246008fb23211195f3a7a688c7e0
marc-antoine <ma.martinod@free.fr>
2013-05-01 16:48:55 +0100

    fitsfile_extractor : delete <<<<<<<<<HEAD ====== >>>>>>>>>>>> origin/master which aren't understood by python.

--- fitsfile_extractor.py
+++ fitsfile_extractor.py
@@ -7,10 +7,7 @@ class FitsFile(object):  # this will hold a fitsfile so its better to call it a 
 
         if fits_file: # The default None argument means we can open a FitsFile object without calling load.
             self.loadfile(fits_file)
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/master
 
     def loadfile(self, fits_file):
         """ I created this method to split the code up a bit and allow me to create a unit test for loading without
