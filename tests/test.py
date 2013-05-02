92f6db469d8c57d35eb4a58b4739dcb7c33ee2e0
marc-antoine <ma.martinod@free.fr>
2013-05-02 11:40:12 +0100

    Test.py Nothing interesting.

--- tests/test.py
+++ tests/test.py
@@ -3,13 +3,10 @@ import unittest
 import numpy
 from math import floor
 
-x1 = 0
-x2 = 2
-y1 = 0
-y2 = 2
 a = numpy.array([[0,1,2],[3,4,5],[6,7,8]])
 
-d = numpy.copy(a[x1:x2,y1:y2])
-print a
 
-print d
+b = a.mean(axis=0).reshape(1,len(a.mean(axis=0)))
+#b = b.reshape(1,len(b))
+print  a
+print b
