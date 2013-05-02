dabafcc81c20fd4d645373639a337dfae36a2719
marc-antoine <ma.martinod@free.fr>
2013-05-02 15:24:39 +0100

    run. To run the plotting.

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,9 @@
 # -*- coding: Latin-1 -*-
-import unittest
-import numpy
-from math import floor
+import numpy as np
+import matplotlib.pyplot as plt
 
-a = numpy.array([[0,1,2],[3,4,5],[6,7,8]])
-
-
-b = a.mean(axis=0).reshape(1,len(a.mean(axis=0)))
-#b = b.reshape(1,len(b))
-print  a
-print b
+y = np.array([[1,2,3]])
+print y[0]
+y = y.tolist()
+print y
+print type(y)
