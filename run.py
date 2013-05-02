c839b49290b8cc36586cd907df1b6ca5fc3dff89
marc-antoine <ma.martinod@free.fr>
2013-05-02 18:04:03 +0100

    run : displays the two spectra.

--- run.py
+++ run.py
@@ -5,9 +5,15 @@ import pylab as pl
 from fitsfile_extractor import FitsFile
 
 
+
 fits = FitsFile("ibxy01aoq_raw.fits")
-pl.imshow(fits.data)
-pl.show
+#pl.imshow(fits.data)
+#pl.show()
 fits.cropBox(62,153,193,242)
-fits.spectrumBox()
-fits.plot(fits.spectrum)
+fits.spectrumBoxHorz()
+pl.figure()
+fits.plot(fits.spectrumHorz)
+
+fits.spectrumBoxVert()
+pl.figure()
+fits.plot(fits.spectrumVert)
--- tests/test.py
+++ tests/test.py
@@ -2,5 +2,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.plot([1,2,4])
+x = np.array([0.0, 1.0, 2.0, 3.0,  4.0,  5.0])
+y = np.array([0.0, 0.8, 0.9, 0.1, -0.8, -1.0])
+z = np.polyfit(y, 3)
+print z
+p = np.poly1d(z)
+p30 = np.poly1d(np.polyfit(x,y,30))
+xp = np.linspace(-2,6,100)
+plt.plot(x,y,'.', xp, p(xp),'-', xp, p30(xp), '--')
+plt.ylim(-2,2)
 plt.show()
+print p(500)
