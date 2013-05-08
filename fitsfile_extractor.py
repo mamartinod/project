48cb82fa216f7162aa990cd0a98f1a241d461399
Ryan Varley <ryanjvarley@gmail.com>
2013-05-08 11:17:29 +0100

    code changes and optimisation

--- fitsfile_extractor.py
+++ fitsfile_extractor.py
@@ -23,14 +23,14 @@ class FitsFile(object):  # this will hold a fitsfile so its better to call it a 
     def zeroReadSubstract(self, data, zeroRead):
         return data - zeroRead
             
-    def spectrum(self, data): #vertical averaging
+    def spectrum(self): #vertical averaging
         name = "Spectrum (summed over the scan)"
-        self.spectrum = Spectrum(data.mean(axis=0), name)
+        self.spectrum = Spectrum(self.data.mean(axis=0), name)
         return self.spectrum
         
-    def spatialSpectrum(self, data): #horizontal averaging
+    def spatialSpectrum(self): #horizontal averaging
         name = "Signal in spatial direction"
-        self.spatialspectrum = Spectrum(data.mean(axis=1), name)
+        self.spatialspectrum = Spectrum(self.data.mean(axis=1), name)
         return self.spatialspectrum
         
     """def plot(self, plotted): #Plot data
@@ -41,8 +41,59 @@ class FitsFile(object):  # this will hold a fitsfile so its better to call it a 
         if plotted.name:
             plt.title(self.spectrumName)
         return plt.show()"""
+        
+        
+class Spectrum(object):
+    def __init__(self, data, name="Plot", xaxis="Pixels", yaxis="Intensity"):
+        self.name = name
+        self.data = data
+        # if xaxis:
+        self.xaxis = xaxis
+        # if yaxis: # These ifs do nothing as you have set defaults in the __init__!
+        self.yaxis = yaxis
+            
+    def _plot(self, data): # Plot data
+        fig = plt.figure()
+        plt.plot(data, 'r+')
+        plt.xlabel(self.xaxis)
+        plt.ylabel(self.yaxis)
+        plt.title(self.name)
+        # return plt.show() # This will halt execution now return the figure isntead
 
-    
+        return fig
+
+
+    def plotData(self):
+        self._plot(self.data)
+
+    def plotExtraction(self):
+        self._plot(self.extraction)
+
+    def autoExtract(self):#Extract signal from a 1D-array
+        """This method derives the signal. The idea is the extrema values of
+        the derivative flank the signal which has to be extracted. It is very
+        sensitive to artefacts so it should deal with a derivative signal which have
+        passed through a median filter which remove artefacts from cosmic rays."""
+
+        array = self.data
+
+        # Here you called the same function twice, call it once and do the methods on it
+        medfilter = self.medianFilter(self.derivation(array))
+        derivmax = medfilter.argmax()
+        derivmin = medfilter.argmin()
+        i = 0
+        extraction = []
+        while i < len(array):
+            if derivmax <= i <= derivmin: # you can simplify to one expression
+                extraction.append(array[i])
+            else:
+                extraction.append(0)
+            i += 1 # You can use += to increment
+        self.extraction = np.array(extraction)
+        self.derivmax = derivmax
+        self.derivmin = derivmin
+        return self.extraction
+
     def derivation(self, array):#Get the derivative of a 1D-array.
         i = 0
         derivative = []
@@ -51,50 +102,10 @@ class FitsFile(object):  # this will hold a fitsfile so its better to call it a 
             i = i+1
             if i >=300:
                 break
-            
+
         self.derivative = np.array(derivative)
         return self.derivative
-        
+
     def medianFilter(self, array):#remove salt-and-pepper noise
         self.medfilt = medfilt(array).copy()
         return self.medfilt
-        
-        
-    def autoExtract(self, array):#Extract signal from a 1D-array
-        """This method derives the signal. The idea is the extrema values of 
-        the derivative flank the signal which has to be extracted. It is very 
-        sensitive to artefacts so it should deal with a derivative signal which have
-        passed through a median filter which remove artefacts from cosmic rays."""
-        
-        derivmax = self.medianFilter(self.derivation(array)).argmax()
-        derivmin = self.medianFilter(self.derivation(array)).argmin()
-        i = 0
-        extraction = []
-        while i < len(array):
-            if i >= derivmax and i <= derivmin:
-                extraction.append(array[i])
-            else:
-                extraction.append(0)
-            i = i+1
-        self.extraction = np.array(extraction)
-        self.derivmax = derivmax
-        self.derivmin = derivmin
-        return self.extraction
-        
-        
-class Spectrum(object):
-    def __init__(self, data, name="Plot", xaxis="Pixels", yaxis="Intensity"):
-        self.name = name
-        self.data = data
-        if xaxis:
-            self.xaxis = xaxis
-        if yaxis:
-            self.yaxis = yaxis
-            
-    def plot(self, plotted): #Plot data
-        plt.figure()
-        plt.plot(plotted, 'r+')
-        plt.xlabel(self.xaxis)
-        plt.ylabel(self.yaxis)
-        plt.title(self.name)
-        return plt.show()
--- run.py
+++ run.py
@@ -12,11 +12,32 @@ fits = FitsFile("ibxy01aoq_raw.fits")
 #pl.imshow(fits.data)
 #pl.show()
 
-fits.spectrum(fits.data)
-fits.spatialSpectrum(fits.data)
-spectralspectrum = fits.autoExtract(fits.spectrum.data)
-spatialspectrum = fits.autoExtract(fits.spatialspectrum.data)
-fits.spectrum.plot(fits.spectrum.data)
-fits.spectrum.plot(spectralspectrum)
-fits.spatialspectrum.plot(fits.spatialspectrum.data)
-fits.spatialspectrum.plot(spatialspectrum)
+"""
+OK so im not certain you've fully grasped the power of classes. When you call a class you create an object or instance
+of that class. That class can store variables and perform methods on itself. For that reason if you are using internal
+data in the class you dont need to give the method that information.
+
+Now since you make a spectrum and then 'optimise' it it makes sense to do this in the spectrum class
+"""
+
+spectralspectrum = fits.spectrum()  # the class already has the data so why give it again?
+spatialspectrum = fits.spatialSpectrum()
+
+spectralspectrum.autoExtract()
+spatialspectrum.autoExtract()
+
+
+
+# fits.spectrum.plot(fits.spectrum.data)
+
+# fits.spatialspectrum.plot(fits.spatialspectrum.data) # this is odd, your asking a class to plot something other than itself
+# fits.spatialspectrum.plot() # With no need for any information in the class. Use a normal function outside the
+ # class for this perhaps and then make the spectrum plot method call that instead of writing the same code twice
+
+spatialspectrum.plotData()
+spatialspectrum.plotExtraction()
+
+spectralspectrum.plotData()
+spatialspectrum.plotExtraction()
+
+pl.show()
